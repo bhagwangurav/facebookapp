c28f83322ddeda5c6182ad28773bed10515b4a00
bhagwan <amol.aloha1@gmail.com>
2014-07-01 21:03:43 +0530

    sign up changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,10 @@ class SessionsController < Devise::SessionsController
    '/users/show'
   end
 
+  def after_sign_up_path_for(resource)
+   '/users/show'
+  end
+
   def after_update_path_for(resource)
     edit_user_registration_path
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  validates :name, presence: true
+  #validates :name, presence: true
 
   has_many :posts
   # attr_accessible :title, :body
